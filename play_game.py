29aac2ad8016141901873923f268d58326e545d4
Dana Kaplan <danamkaplan@gmail.com>
2017-03-20 13:49:27 -0700

    cleaned up play script

--- play_game.py
+++ play_game.py
@@ -1,28 +1,6 @@
-from pysweeper import Cell, Grid, Game
-# import curses
-# import curses.textpad
-# import time
+from pysweeper import Game
 
-#print "Pres"
-'''
-rows = raw_input("How many rows would you like? (Press enter for 8) ")
-cols = raw_input("How many columns would you like? (Press enter for 8) ")
-bombs = raw_input("How many bombs would you like? (Press enter for 9) ")
-
-print type(rows)
-print cols
-print bombs
-''' 
-
+# Create Game object and play
 game = Game()
 game.create_game()
-
-#n_dict = grid.get_neighbor_dict()
-
-#game.draw_screen()
 game.play_game()
-
-#window = curses.initscr()
-#curses.endwin()
-
-
